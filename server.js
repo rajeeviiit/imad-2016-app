e2b31279af13f060a8c90ba54fc83f7b90eef807
R@jeev <singhrajiv774@gmail.com>
2016-10-06 00:34:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var ArticleOne = {
+var articleOne = {
 title:'Article One | Rajeev',
 heading:'articleOne',
 date:'sep 5, 2016',
