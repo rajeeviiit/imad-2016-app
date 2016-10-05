df0ee834dd3c1f1b4552e3042cc31ccf400c0f86
R@jeev <singhrajiv774@gmail.com>
2016-10-06 00:59:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleName', function (req, res) {
-    var =req.params.articleName;
+    var articleName =req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
