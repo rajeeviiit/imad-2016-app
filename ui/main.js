5052ae055d680bbbfae9ccf56085bd3eb4ee5aeb
R@jeev <singhrajiv774@gmail.com>
2016-10-07 00:47:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ element.innerHTML='Rajeev Singh';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
     
 }
