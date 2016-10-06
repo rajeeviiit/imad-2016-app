2027a8a9c31f76f9d9007b1223969d5a8283c552
R@jeev <singhrajiv774@gmail.com>
2016-10-07 00:26:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element =document.getElementById('main-text');
 element.innerHTML='Rajeev Singh';
-var img=document.getElementById('img');
+var img=document.getElementById('modi');
 img.onClick=function(){
     
     img.style.marginleft="100px";
