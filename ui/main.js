fc1c8630501e4fa130af91841429b0193e2e11fa
R@jeev <singhrajiv774@gmail.com>
2016-10-07 11:26:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,13 +42,30 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-  var names =['name1','name2','name3','name4'];
-  var list='';
-  for(var i=0;i<names.length;i++){
-      list +='<li>' +names[i] + '</li>';
-  }
-  var ul =document.getElementById('namelist');
-  ul.innerHTML=list;
+    
+     var request= new XMLHttpRequest();
+    
+    //capture the response and store it in variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //Take action
+                if(request.status===200){
+                 var names=JSON.parse(names);
+                 
+                 var list='';
+                 for(var i=0;i<names.length;i++){
+                 list +='<li>' +names[i] + '</li>';
+      }
+      var ul =document.getElementById('namelist');
+      ul.innerHTML=list;
+            }
+        }
+        //do not yet
+    };
+    //make the request
+    request.open('GET','http://rajeeviiit.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+ 
 };
 
 
