f9eb02770480489ea50ed7e37cd76e0f52cb6e02
R@jeev <singhrajiv774@gmail.com>
2016-10-07 11:13:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/counter',function(req, res){
     counter = counter+1;
     res.send(counter.toString());
 });
-
+var names=[];
 app.get('/submit-name',function(req, res){
     var name=req.params.name;
     names.push(name);
