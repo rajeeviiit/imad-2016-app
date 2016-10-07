c4c6a402b755e7e9a31287f248c7c8c07913342a
R@jeev <singhrajiv774@gmail.com>
2016-10-07 09:58:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,16 @@ img.onclick=function(){
     
 };
 
+//counter code
+var button =document.getElementById('counter');
+var counter =0;
+button.onclick=function(){
+    //make the request to the counter endpoint
+    
+    //capture the response and store it in variable
+    
+    //render the variable in correct span
+    counter =counter +1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
