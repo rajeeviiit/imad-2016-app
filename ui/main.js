a02fbeea577377ba10bc0a58a35231ee2f40207e
R@jeev <singhrajiv774@gmail.com>
2016-10-07 10:14:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,4 @@
-console.log('Loaded!');
-var element =document.getElementById('main-text');
-element.innerHTML='Rajeev Singh';
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-    
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-    
-};
+
 
 //counter code
 var button =document.getElementById('counter');
